aa8926fd188a5649954a9e46cc2806758dc3e2e7
gdsoto1991 <dulcey1991@gmail.com>
2024-09-05 13:58:21 -0400

    Se elimino el contador oculto y se actualizaron los archivos

--- src/assets/js/countdown.js
+++ src/assets/js/countdown.js
@@ -1,31 +1,29 @@
 simplyCountdown('#cuenta', {
-	year: 2024, // required
-	month: 3, // required
-	day: 9, // required
-	hours: 7, // Default is 0 [0-23] integer
-	minutes: 31, // Default is 0 [0-59] integer
-	seconds: 0, // Default is 0 [0-59] integer
-	words: {
-		//words displayed into the countdown
-		days: 'Día',
-		hours: 'Hora',
-		minutes: 'Minuto',
-		seconds: 'Segundo',
-		pluralLetter: 's',
-	},
-	plural: true, //use plurals
-	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
-	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
-	// in case of inline set to false
-	enableUtc: true, //Use UTC as default
-	onEnd: function () {
-		document.getElementById('portada').classList.add('oculta');
-		return;
-	}, //Callback on countdown end, put your own function here
-	refresh: 1000, // default refresh every 1s
-	sectionClass: 'simply-section', //section css class
-	amountClass: 'simply-amount', // amount css class
-	wordClass: 'simply-word', // word css class
-	zeroPad: false,
-	countUp: false,
+    year: 2024,
+    month: 3,
+    day: 9,
+    hours: 7,
+    minutes: 31,
+    seconds: 0,
+    words: {
+        days: 'Día',
+        hours: 'Hora',
+        minutes: 'Minuto',
+        seconds: 'Segundo',
+        pluralLetter: 's',
+    },
+    plural: true,
+    inline: false,
+    inlineClass: 'simply-countdown-inline',
+    enableUtc: true,
+    onEnd: function () {
+        // Deja la función vacía o añade alguna acción sin ocultar el contador
+    },
+    refresh: 1000,
+    sectionClass: 'simply-section',
+    amountClass: 'simply-amount',
+    wordClass: 'simply-word',
+    zeroPad: false,
+    countUp: false,
 });
+
